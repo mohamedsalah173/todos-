fc96d3057c14f722c2e6ede3e6b9a23522c5813d
Mohamed Salah <mohamedsalahmasoud173@gmail.com>
2023-03-14 05:15:51 +0200

    updating relations between routers and controllers

--- controllers/users.js
+++ controllers/users.js
@@ -1,9 +1,19 @@
+/* eslint-disable no-undef */
 /* eslint-disable import/no-self-import */
 
 const Users = require('../models/users');
 
 const create = (data) => Users.create(data);
 
+const get = () => Users.find();
+
+const patch = (id, fName) => Users.updateOne({ _id: id }, { fName });
+
+const remove = (id, fName) => Users.deleteOne({ _id: id });
+
 module.exports = {
   create,
+  get,
+  patch,
+  remove,
 };
--- routers/users.js
+++ routers/users.js
@@ -1,3 +1,4 @@
+/* eslint-disable no-undef */
 /* eslint-disable no-unused-vars */
 const express = require('express');
 const { userController } = require('../controllers');
@@ -13,4 +14,36 @@ router.post('/', async (req, res) => {
     res.json(error.message);
   }
 });
+
+router.get('/', async (req, res) => {
+//   const { query: { userName, fName, lName } } = req;
+  try {
+    const user = await userController.get();
+    res.json(user);
+  } catch (error) {
+    res.json(error.message);
+  }
+});
+
+router.patch('/:id', async (req, res) => {
+  const { params: { id } } = req;
+  const { body: { userName, fName, lName } } = req;
+
+  try {
+    const user = await userController.patch(id, fName);
+    res.json(user);
+  } catch (error) {
+    res.json(error.message);
+  }
+});
+router.delete('/:id', async (req, res) => {
+  const { params: { id } } = req;
+  try {
+    const user = await userController.remove(id);
+    res.json(user);
+  } catch (error) {
+    res.json(error.message);
+  }
+});
+
 module.exports = router;
